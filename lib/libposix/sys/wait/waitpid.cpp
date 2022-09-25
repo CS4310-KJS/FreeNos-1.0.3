cd7e2badd7ca55e5d27302eda1e46c343d04d30b
shaghik <78176149+shaghik@users.noreply.github.com>
2022-09-25 11:39:08 -0700

    few changes
    Signed-off-by: shaghik <78176149+shaghik@users.noreply.github.com>

--- lib/libposix/sys/wait/waitpid.cpp
+++ lib/libposix/sys/wait/waitpid.cpp
@@ -16,6 +16,9 @@
  */
 
 #include <FreeNOS/User.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "sys/wait.h"
 #include "sys/types.h"
 #include <errno.h>
@@ -23,11 +26,11 @@
 pid_t waitpid(pid_t pid, int *stat_loc, int options)
 {
     const ulong result = (ulong) ProcessCtl(pid, WaitPID);
-
     switch ((const API::Result) (result & 0xffff))
     {
         case API::NotFound:
             errno = ESRCH;
+            ERROR("Can not wait : " << strerror(errno));
             return (pid_t) -1;
 
         case API::Success:
