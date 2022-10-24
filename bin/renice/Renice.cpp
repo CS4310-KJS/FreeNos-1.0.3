950bdec3da8f8b20e1b1c9a77adffe4d8ab6fc2c
shaghik <78176149+shaghik@users.noreply.github.com>
2022-10-24 15:19:00 -0700

    Update Renice.cpp
    Signed-off-by: shaghik <78176149+shaghik@users.noreply.github.com>

--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -1,40 +1,59 @@
+/*
+ * Copyright (C) 2009 Niek Linnenbank
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ProcessClient.h>
+#include <errno.h>
 #include <unistd.h>
-#include <Process.h>
+#include <FreeNOS/User.h>
 #include "Renice.h"
 
-int prio;
-ProcessID pid;
-
-renice::renice(int argc, char **argv) : POSIXApplication(argc, argv){
-    parser().setDescription("Change the priority of a process");
-    parser().registerFlag('n', "setPriority", "Changes process priority to new priority");
-    parser().registerPositional("PRI", "Priority to change the process to");
-    parser().registerPositional("PID", "PID of the process to change");
-    prio = atoi(argv[2]);
-    pid = atoi(argv[3]);
+Renice::Renice(int argc, char **argv)
+    : POSIXApplication(argc, argv)
+{
+    parser().setDescription("Change the priority level of a process");
+    parser().registerFlag('n', "prio", "optional flag for setting priority");
+    parser().registerPositional("priority", "new priority level for the process (1-5)");
+    parser().registerPositional("process_id", "ID of the process to change priority for");
 }
 
-renice::~renice(){
-
+Renice::~Renice() {
 }
 
-renice::Result renice::exec(){
-    ProcessClient process;
-    if (pid <=  0 || pid >= ProcessClient::MaximumProcesses)
-    {
-        ERROR("Invalid PID: " << pid);
+Renice::Result Renice::exec(){
+
+    unsigned int priority = atoi(arguments().get("priority"));
+
+    if (priority < 1 || priority > 5){
+        ERROR("invalid priority level `" << priority << "'");
         return InvalidArgument;
-    } else if (prio <= 0 || prio > 5){
-        ERROR("Invalid priority: " << prio);
-        return InvalidArgument;
-    } else {
-        process.setPriority(pid, prio);
-        //out << "Priority Changed!!!!!!!!!!\r\n";
     }
 
-    return Success;
+    pid_t pid = atoi(arguments().get("process_id"));
+
+    if (pid < 0){
+        ERROR("invalid process id");
+        return InvalidArgument;
+    }
+
+    if (setprio(pid, priority, 0) != pid)
+    {
+        ERROR("renice failed: " << strerror(errno));
+        return IOError;
+    }
 }
