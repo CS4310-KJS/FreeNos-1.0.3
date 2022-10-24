c311cced77c2dd5bc3eed48bae964afb1257b924
shaghik <78176149+shaghik@users.noreply.github.com>
2022-10-23 17:16:21 -0700

    Update Renice.h
    Signed-off-by: shaghik <78176149+shaghik@users.noreply.github.com>

--- bin/renice/Renice.h
+++ bin/renice/Renice.h
@@ -1,5 +1,22 @@
-#ifndef __BIN_RENICE_H
-#define __BIN_RENICE_H
+/*
+ * Copyright (C) 2009 Niek Linnenbank
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __BIN_RENICE_RENICE_H
+#define __BIN_RENICE_RENICE_H
 
 #include <POSIXApplication.h>
 
@@ -9,11 +26,11 @@
  */
 
 /**
- * Change Priority Level of a process.
+ * Wait for a process to terminate
  */
 class Renice : public POSIXApplication
 {
-  public:
+public:
 
     /**
      * Constructor
@@ -34,11 +51,10 @@ class Renice : public POSIXApplication
      * @return Result code
      */
     virtual Result exec();
-
 };
 
 /**
  * @}
  */
 
-#endif /* __BIN_RENICE_H */
+#endif /* __BIN_RENICE_RENICE_H */
